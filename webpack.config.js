21488440d219c41aeb76a67c3571159630966871
DARKLORD82 <TiksiHehku@outlook.com>
2025-05-11 21:48:14 +0300

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './App.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'web-build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
