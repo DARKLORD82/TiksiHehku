1f1f9fdd842771b19c3342e4d221743fa462d7e4
DARKLORD82 <TiksiHehku@outlook.com>
2025-05-11 19:45:01 +0300

    App.js

--- App.js
+++ App.js
@@ -1,5 +1,1 @@
-HEAD
-// Firebase Auth + Navigaatio TiksiHehku
-// Täydellinen versio ladattavaksi – sisältää drawerin, kirjautumisen ja näkymät.
-// TiksiHehku App
-console.log('Tervetuloa TiksiHehkuun');
+
