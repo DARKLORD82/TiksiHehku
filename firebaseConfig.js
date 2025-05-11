318de6b011e0a3190397cb61b697f156a51774e3
Jani Aalto <TiksiHehku@outlook.com>
2025-05-11 20:25:42 +0300

    Lisätty täysi TiksiHehku App.js käyttöliittymällä

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,8 @@
-// Täydennä Firebase-konfiguraatio näin:
-// export const firebaseConfig = {
-//   apiKey: "...",
-//   authDomain: "...",
-//   projectId: "...",
-//   storageBucket: "...",
-//   messagingSenderId: "...",
-//   appId: "..."
-// };
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
