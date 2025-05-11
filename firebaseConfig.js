82dd85ef688b54681c18c1bb8a5f2dcfc3841f60
DARKLORD82 <TiksiHehku@outlook.com>
2025-05-11 19:17:34 +0300

    firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
+ HEAD
 // Täydennä oma Firebase-konfiguraatio tähän
-=======
+
 // Firebase-konfiguraatio tähän
->>>>>>> 61ad5ed06f5d53b338580ac1a1b30550aa015ccc
+61ad5ed06f5d53b338580ac1a1b30550aa015ccc
